ee97e1459ad5e2f408ec4329b2c9770ea9be3b74
beluvr <beluuvilla@gmail.com>
2024-08-29 01:32:09 -0300

    Añadido estilo y funcionalidad al div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const miDiv = document.getElementById('miDiv');
+    
+    miDiv.addEventListener('click', () => {
+        alert('Hola! Soy el div');
+    });
+});
